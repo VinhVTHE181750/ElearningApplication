23abaf5d33533b296a73d216346c937413fe5bd8
minhnhhe161877 <145969597+minhnhhe161877@users.noreply.github.com>
2024-06-07 01:27:46 +0700

    SWTL1-2

--- src/test/java/team2/elearningapplication/ElearningSwp391ApplicationTests.java
+++ src/test/java/team2/elearningapplication/ElearningSwp391ApplicationTests.java
@@ -1,10 +1,15 @@
 package team2.elearningapplication;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ElearningSwp391ApplicationTests {
+
+    @Autowired
+    private LoginService loginService; // Assuming you have a LoginService
 
     @Test
     void contextLoads() {
@@ -12,7 +17,15 @@ class ElearningSwp391ApplicationTests {
 
     @Test
     void testLoginService() {
+        // Given
+        String username = "testuser";
+        String password = "testpassword";
 
+        // When
+        boolean loginResult = loginService.login(username, password);
+
+        // Then
+        assertTrue(loginResult, "Login should be successful for valid credentials");
     }
 
 }
