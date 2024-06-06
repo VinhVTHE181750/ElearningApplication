2b42fab06b626666f101febe71b71149145cf1eb
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-06-06 15:38:42 +0700

    SWTL1-24

--- src/main/java/team2/elearningapplication/ElearningSwp391Application.java
+++ src/main/java/team2/elearningapplication/ElearningSwp391Application.java
@@ -2,7 +2,6 @@ package team2.elearningapplication;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +21,6 @@ public class ElearningSwp391Application {
                 "api_secret", "***************************",
                 "secure",true));
         return cloudinary;
-    };
+    }
 
 }
