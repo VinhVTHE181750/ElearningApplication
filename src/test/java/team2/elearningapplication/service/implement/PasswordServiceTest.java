c601b75bb843db378555b766a49014e49d9e32b9
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-06-26 11:32:15 +0700

    Update PasswordServiceTest.java

--- src/test/java/team2/elearningapplication/service/implement/PasswordServiceTest.java
+++ src/test/java/team2/elearningapplication/service/implement/PasswordServiceTest.java
@@ -15,12 +15,20 @@ class PasswordServiceTest {
     }
 
     @Test
-    void hassPasswordNullString() {
+    void hashPasswordNullString() {
         PasswordService passwordService = new PasswordService();
         String password = null;
         String hashedPassword = passwordService.hashPassword(password);
         assertThrows(NullPointerException.class, () -> {
             throw new NullPointerException();
         });
+    }
+
+    @Test
+    void hashPasswordEmpty() {
+        PasswordService passwordService = new PasswordService();
+        String password = "";
+        String hashedPassword = passwordService.hashPassword(password);
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", hashedPassword);
     }
 }
