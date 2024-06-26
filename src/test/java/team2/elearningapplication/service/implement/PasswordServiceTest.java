0565985bfe91ab821bbe881f0d9e6816d7eebc72
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-06-26 11:30:16 +0700

    Create PasswordServiceTest.java

---
+++ src/test/java/team2/elearningapplication/service/implement/PasswordServiceTest.java
@@ -0,0 +1,26 @@
+package team2.elearningapplication.service.implement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordServiceTest {
+
+    @Test
+    void hashPassword() {
+        PasswordService passwordService = new PasswordService();
+        String password = "password";
+        String hashedPassword = passwordService.hashPassword(password);
+        assertEquals("5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", hashedPassword);
+    }
+
+    @Test
+    void hassPasswordNullString() {
+        PasswordService passwordService = new PasswordService();
+        String password = null;
+        String hashedPassword = passwordService.hashPassword(password);
+        assertThrows(NullPointerException.class, () -> {
+            throw new NullPointerException();
+        });
+    }
+}
